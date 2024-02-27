a8437dc2734a6bf764bb0f56dac118ea88875057
briato <brunoduarte9798@gmail.com>
2024-02-27 17:51:09 +0000

    Ficheiros iniciais

---
+++ dailytemp.c
@@ -0,0 +1,28 @@
+/** \file dailytemp.c
+ * \brief Ficheiro para colocar o código da aula prática 1, 
+ * serve para fazer algumas coisas com temperatura
+ *
+ *
+ * \author Bruno Duarte
+ * \author Diogo Lapa
+ *
+ * \date 27-02-2024
+ * \bug Não tem bugs, somos uns bichos
+ *
+ * */
+
+
+
+int* dtInit() {
+
+	vect	
+
+
+
+
+}
+
+
+
+
+
---
+++ dailytemp.h
@@ -0,0 +1,10 @@
+#ifndef __DAILYTEMP_H__
+#define __DAILYTEMP_H__
+
+int* dtInit();
+int dtAdd(int* vect, int value);
+int* dtStat(int* vect) ;
+
+#endif
+
+
